1014f83a90f913745abcfa5ecdc60e37155c2000
adarshkreditbee <adarsh.agarwal@kreditbee.in>
2021-04-30 14:30:56 +0530

    integrate eslint to the current project

--- react-complete-guide1/src/App.js
+++ react-complete-guide1/src/App.js
@@ -1,15 +1,19 @@
-import "./App.css";
-import NewExpense from "./components/NewExpense/NewExpense";
-import Expenses from "./components/Expenses/Expenses";
-import Wrapper from "./components/Helpers/Wrapper";
-import Foobar from "./components/Examples/Foobar";
+import React from 'react';
+import './App.css';
+import NewExpense from './components/NewExpense/NewExpense';
+import Expenses from './components/Expenses/Expenses';
+import Wrapper from './components/Helpers/Wrapper';
+import Foobar from './components/Examples/Foobar';
 
 function App() {
   const expenses = [
-    { date: new Date(2021, 2, 29), title: "Car Insurance", price: 21000 },
-    { date: new Date(2020, 2, 29), title: "Food and drinks", price: 410 },
-    { date: new Date(2019, 2, 29), title: "Grocery", price: 1300 },
+    { date: new Date(2021, 2, 29), title: 'Car Insurance', price: 21000 },
+    { date: new Date(2020, 2, 29), title: 'Food and drinks', price: 410 },
+    { date: new Date(2019, 2, 29), title: 'Grocery', price: 1300 },
   ];
+
+  // eslint-disable-next-line no-console
+  console.log('HELLO');
 
   return (
     <Wrapper>
